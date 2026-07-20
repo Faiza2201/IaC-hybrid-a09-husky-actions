cedc043a66eef887a1355f661af9a60187351a00
faiza2201 <boud0219@algonquinlive.com>
2026-07-19 22:19:37 -0400

    Initial commit

---
+++ infrastructure/main.tf
@@ -0,0 +1,28 @@
+# infrastructure/main.tf
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+                resource "azurerm_resource_group" "rg" {
+  name     = "lab9Husky-rg"
+  location = "Canada Central"
+}
+
+resource "azurerm_storage_account" "storageterraformname" {
+  name                     = "strlab9husky001"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
